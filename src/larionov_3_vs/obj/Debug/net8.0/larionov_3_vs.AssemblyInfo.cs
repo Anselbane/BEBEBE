07ddff0d826486c11f3a03f090e49b170123fb96
TiamatBronze1 <95623386+TiamatBronze1@users.noreply.github.com>
2024-12-24 15:05:25 +0300

    alo

--- src/larionov_3_vs/obj/Debug/net8.0/larionov_3_vs.AssemblyInfo.cs
+++ src/larionov_3_vs/obj/Debug/net8.0/larionov_3_vs.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("larionov_3_vs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a060587958edc25eaed1e3ce654f48912c865112")]
 [assembly: System.Reflection.AssemblyProductAttribute("larionov_3_vs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("larionov_3_vs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
